5b8706542491db88c189eeca578190981d261907
funkee <xxtrm98@protonmail.com>
2023-10-31 18:36:06 -0400

    menu button

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,16 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { links } from "../../../lib/data";
 import { motion } from "framer-motion";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { TfiClose } from "react-icons/tfi";
 
 function Header() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const handleMenu = () => {
+    setOpen((prevState) => !prevState);
+  };
   return (
     <motion.div
       className="flex p-4 border-b-[1px] border-neutral-100 items-center justify-between"
@@ -15,13 +22,25 @@ function Header() {
           Logo
         </div>
       </div>
-      <nav className="hidden sm:flex px-2 gap-2 text-lg">
-        <div className="flex">
+      <nav className="px-2 gap-2 text-lg">
+        <div className="hidden  md:flex lg:flex">
           {links.map((link) => (
-            <motion.ul className="px-2 py-1" key={link.hash}>
+            <motion.ul
+              className="px-3 py-1 transition-all duration-300 hover:text-white cursor-pointer"
+              key={link.hash}
+            >
               {link.name}
             </motion.ul>
           ))}
+        </div>
+        <div className="-mr-0 flex  md:hidden lg:hidden xl:hidden">
+          <button
+            type="button"
+            onClick={handleMenu}
+            className="items-center border-black border-[2px] p-2 transition-all duration-400 hover:bg-gray-700 hover:text-white"
+          >
+            {open ? <TfiClose /> : <RxHamburgerMenu />}
+          </button>
         </div>
       </nav>
     </motion.div>
