d706dfdf418080db501338645c519e2224cb0cc5
funkee <xxtrm98@protonmail.com>
2023-10-31 10:21:37 -0400

    pushing basic root layout styling and lib data for test

---
+++ lib/data.ts
@@ -0,0 +1,30 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+];
+
+export const skillsData = [
+  "HMTL",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Express",
+  "Git",
+  "MongoDB",
+  "Redux",
+  "Tailwind",
+  "Sass",
+];
