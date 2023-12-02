200935bfd5186b56296ca61d217f8cca0012af46
funkee <xxtrm98@protonmail.com>
2023-12-02 10:45:47 -0800

    adding text to home

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -8,15 +8,23 @@ export default function Intro() {
   return (
     <section>
       <motion.div
-        className="flex px-5 mt-[7rem] justify-center 
+        className="flex px-5 mt-12  justify-center 
         xs:flex-col-reverse xs:justify-center xs:items-center 
         md:flex-row md:justify-between"
         initial={{ y: 25, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <div className="mt-5">Testing</div>
         <div>
+          <div className="text-center md:text-left">
+            <h1 className="group my-4 text-3xl font-semibold">Hello!</h1>
+          </div>
+          <p className="my-3 text-center md:text-left">My name is Armando.</p>
+          <p className="my-3 text-center md:text-left">
+            I&apos;m a software developer.
+          </p>
+        </div>
+        <div className="flex">
           <Image
             className="rounded-xl shadow-xl"
             height={350}
